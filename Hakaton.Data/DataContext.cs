c694aa36f6a38523e877d77698fc0f5718f9ca7a
Влад Пашечко <vpashechko38@gmail.com>
2019-11-30 10:17:07 +0300

    пуш в мастер

---
+++ Hakaton.Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hakaton.Data
+{
+    public class DataContext : DbContext
+    {
+
+
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        { }
+        
+    }
+}
---
+++ Hakaton/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Hakaton.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hakaton
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration["Connection:ConnectionString"];
+            AddContext(services, connectionString);
+
+            AddCors(services);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceScopeFactory scopeFactory, IServiceProvider serviceProvider)
+        {
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetService<DataContext>();
+                context.Database.Migrate();
+            }
+
+            app.UseCors("MyPolicy");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+
+        /// <summary>
+        /// Регистрация корсов
+        /// </summary>
+        private static void AddCors(IServiceCollection services)
+        {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.WithOrigins("http://localhost:80", "http://localhost:5000", "http://booking.astralnalog.ru:80")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+        }
+
+        /// <summary>
+        /// Регистрация контекста БД
+        /// </summary>
+        private static void AddContext(IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<DataContext>(
+                options => options.UseNpgsql(connectionString,
+                    npgSqlOptions =>
+                    {
+                        npgSqlOptions.MigrationsAssembly(typeof(DataContext).Assembly.GetName()
+                            .Name);
+                    }));
+        }
+    }
+}
